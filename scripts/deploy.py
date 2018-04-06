ec21b91f41431d77a72da95c4984a073cc7c3ff3
Aaron Coburn <acoburn@apache.org>
2018-04-06 15:42:47 -0400

    Add deployment script

---
+++ scripts/deploy.py
@@ -0,0 +1,47 @@
+import argparse
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+LOGGER = logging.getLogger('trellis')
+LOGGER.setLevel(logging.INFO)
+
+
+class Uploader:
+
+    def __init__(self, bucket, profile="default"):
+        self.s3 = boto3.Session(profile_name=profile).client('s3')
+        self.bucket = bucket
+
+    def upload(self, path, key, mime_type):
+        LOGGER.info("Uploading {} as {}".format(path, mime_type))
+        try:
+            with open(path, 'rb') as fp:
+                self.s3.upload_fileobj(fp, self.bucket, key, ExtraArgs={
+                    'ContentType': mime_type})
+        except ClientError as e:
+            LOGGER.warn("Error writing {}: {}".format(path, e))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+            description="Upload ontology files to S3")
+    parser.add_argument("--bucket", help="The bucket name")
+    parser.add_argument("--profile", default="default",
+                        help="The credentials profile")
+
+    args = parser.parse_args()
+
+    uploader = Uploader(bucket=args.bucket, profile=args.profile)
+
+    uploader.upload("doap.ttl", "doap.ttl", "text/turtle")
+    uploader.upload("trellis.ttl", "ns/trellis.ttl", "text/turtle")
+    uploader.upload("trellis.html", "ns/trellis.html", "text/html")
+    uploader.upload("trellis.jsonld", "ns/trellis.jsonld",
+                    "application/ld+json")
+    uploader.upload("trellis.rdf", "ns/trellis.rdf", "application/rdf+xml")
+    uploader.upload("trellisresource.jsonld", "ns/trellisresource.jsonld",
+                    "application/ld+json")
+    uploader.upload("css/style.css", "ns/css/style.css", "text/css")
