0f980bdfda2234e269977db0cfca4dcb1741fce6
Aaron Coburn <acoburn@apache.org>
2018-05-30 10:40:14 -0400

    Simplify deployer

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import os
 
 import boto3
 from botocore.exceptions import ClientError
@@ -9,16 +10,18 @@ LOGGER = logging.getLogger('trellis')
 
 class Uploader:
 
-    def __init__(self, bucket, profile="default"):
+    def __init__(self, bucket, profile="default", prefix="ns/"):
         self.s3 = boto3.Session(profile_name=profile).client('s3')
         self.bucket = bucket
+        self.prefix = prefix
 
-    def upload(self, path, key, mime_type):
+    def upload(self, path, mime_type):
         LOGGER.info("Uploading {} as {}".format(path, mime_type))
         try:
             with open(path, 'rb') as fp:
-                self.s3.upload_fileobj(fp, self.bucket, key, ExtraArgs={
-                    'ContentType': mime_type})
+                self.s3.upload_fileobj(fp, self.bucket,
+                                       os.path.join(self.prefix, path),
+                                       ExtraArgs={'ContentType': mime_type})
         except ClientError as e:
             LOGGER.warn("Error writing {}: {}".format(path, e))
 
@@ -38,14 +41,11 @@ if __name__ == '__main__':
 
     uploader = Uploader(bucket=args.bucket, profile=args.profile)
 
-    uploader.upload("trellis.ttl", "ns/trellis.ttl", "text/turtle")
-    uploader.upload("trellis.html", "ns/trellis.html", "text/html")
-    uploader.upload("trellis.jsonld", "ns/trellis.jsonld",
-                    "application/ld+json")
-    uploader.upload("trellis.rdf", "ns/trellis.rdf", "application/rdf+xml")
-    uploader.upload("trellisresource.jsonld", "ns/trellisresource.jsonld",
-                    "application/ld+json")
-    uploader.upload("css/style.css", "ns/css/style.css", "text/css")
-    uploader.upload("img/trellis.png", "ns/img/trellis.png", "image/png")
-    uploader.upload("img/trellis_white.png", "ns/img/trellis_white.png",
-                    "image/png")
+    uploader.upload("trellis.ttl", "text/turtle")
+    uploader.upload("trellis.html", "text/html")
+    uploader.upload("trellis.jsonld", "application/ld+json")
+    uploader.upload("trellis.rdf", "application/rdf+xml")
+    uploader.upload("trellisresource.jsonld", "application/ld+json")
+    uploader.upload("css/style.css", "text/css")
+    uploader.upload("img/trellis.png", "image/png")
+    uploader.upload("img/trellis_white.png", "image/png")
